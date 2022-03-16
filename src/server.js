779fe734eb52e7cbb3975198ec59ce5443c7a230
EvanSamuelW <70187319+EvanSamuelW@users.noreply.github.com>
2022-03-16 12:29:26 +0700

    data validation implemented

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const NotesValidator = require('./validator/notes');
 const NotesService = require('./services/inMemory/NotesService');
 
 const init = async() => {
@@ -20,6 +21,7 @@ const init = async() => {
         plugin: notes,
         options: {
             service: notesService,
+            validator: NotesValidator,
         },
     });
 
