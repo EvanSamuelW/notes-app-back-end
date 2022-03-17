c3f302306da59e84f58e7afe7671d34ba29feac3
EvanSamuelW <70187319+EvanSamuelW@users.noreply.github.com>
2022-03-17 16:35:42 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesValidator = require('./validator/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
+
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async() => {
     const notesService = new NotesService();
+    const usersService = new UsersService();
 
     const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
+        port: process.env.PORT,
+        host: process.env.HOST,
         routes: {
             cors: {
                 origin: ['*'],
@@ -17,13 +24,21 @@ const init = async() => {
     });
 
 
-    await server.register({
-        plugin: notes,
-        options: {
-            service: notesService,
-            validator: NotesValidator,
+    await server.register([{
+            plugin: notes,
+            options: {
+                service: notesService,
+                validator: NotesValidator,
+            },
         },
-    });
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+    ]);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
